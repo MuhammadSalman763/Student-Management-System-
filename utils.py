f5fd8238e92d31828e1553804d6eb559b6832478
Muhammad Salman <salmanfateh12@gmail.com>
2026-07-15 15:59:53 +0500

    age and cgpa validation

--- utils.py
+++ utils.py
@@ -1,8 +1,10 @@
-def validate_age(age):
+def validate_id(student_id):
+    return student_id > 0
 
-    return age > 0
+
+def validate_age(age):
+    return 18 <= age <= 100
 
 
 def validate_cgpa(cgpa):
-    
     return 0 <= cgpa <= 4
