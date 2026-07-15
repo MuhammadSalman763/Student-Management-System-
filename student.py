09e11beb22755c143072208713e22831e1adae5b
Muhammad Salman <salmanfateh12@gmail.com>
2026-07-15 15:57:55 +0500

    initialize the parameters

--- student.py
+++ student.py
@@ -1,6 +1,13 @@
 class Student:
 
-    def __init__(self, student_id, name, age, department, cgpa):
+    def __init__(
+        self,
+        student_id: int,
+        name: str,
+        age: int,
+        department: str,
+        cgpa: float
+    ):
 
         self.student_id = student_id
         self.name = name
@@ -9,13 +16,17 @@ class Student:
         self.cgpa = cgpa
 
     def to_dict(self):
-        
+
         return {
 
             "ID": self.student_id,
+
             "Name": self.name,
+
             "Age": self.age,
+
             "Department": self.department,
+
             "CGPA": self.cgpa
 
         }
