b25907e14c113d20433b3cf2a34af5828e67ce6b
Muhammad Salman <salmanfateh12@gmail.com>
2026-07-15 16:01:51 +0500

    set the lenght of the variables

--- models.py
+++ models.py
@@ -3,8 +3,12 @@ from pydantic import BaseModel, Field
 
 class StudentModel(BaseModel):
 
-    student_id: str
+    student_id: int = Field(..., gt=0)
+
     name: str
-    age: int = Field(..., ge=18, le=30)
+
+    age: int = Field(..., ge=18, le=100)
+
     department: str
+
     cgpa: float = Field(..., ge=0.0, le=4.0)
